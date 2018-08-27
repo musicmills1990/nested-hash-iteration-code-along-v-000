7363191c18eeffba9afe556786c4b9d8ac8cf377
Marc Mills <mdmills1990@gmail.com>
2018-08-27 18:59:58 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -9,7 +9,7 @@ require 'pry'
      },
     "Freddy Mercury" => {
       name: "Freddy",
-     email: "freddy@mercury.com",
+      email: "freddy@mercury.com",
       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
    }
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
-        binding.pry
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
     end
   end
